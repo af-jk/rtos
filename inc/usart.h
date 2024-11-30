6ea940113f6c101f4522118122b7dad8374eadf8
fred13kim <fred13kim@gmail.com>
2024-11-29 20:10:41 -0500

    Add USART_CR1 Register defines

--- inc/usart.h
+++ inc/usart.h
@@ -4,6 +4,10 @@
 
 #define USART2_BASE (APB1PERIPH_BASE + 0x00004400UL)
 
+/**
+ * @brief USART port
+ */
+
 typedef struct {
     volatile uint32_t USART_SR;
     volatile uint32_t USART_DR;
@@ -14,4 +18,24 @@ typedef struct {
     volatile uint32_t USART_GTPR;
 } USART_t;
 
+typedef struct USART_handle_s{
+    USART_t *port;
+} USART_handle_t;
+
 #define USART2 ((USART_t *)USART2_BASE);
+
+/** CR1 Register Bit Defines **/
+#define USART_CR1_SBK    (0x1UL) << (0U)
+#define USART_CR1_RWU    (0x1UL) << (1U)
+#define USART_CR1_RE     (0x1UL) << (2U)
+#define USART_CR1_TE     (0x1UL) << (3U)
+#define USART_CR1_IDLEIE (0x1UL) << (4U)
+#define USART_CR1_RXNEIE (0x1UL) << (5U)
+#define USART_CR1_TCIE   (0x1UL) << (6U)
+#define USART_CR1_PEIE   (0x1UL) << (7U)
+#define USART_CR1_PS     (0x1UL) << (8U)
+#define USART_CR1_PCE    (0x1UL) << (9U)
+#define USART_CR1_WAKE   (0x1UL) << (10U)
+#define USART_CR1_M      (0x1UL) << (11U)
+#define USART_CR1_UE     (0x1UL) << (12U)
+#define USART_CR1_OVER8  (0x1UL) << (13U)
